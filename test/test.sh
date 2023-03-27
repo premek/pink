bc65137eb7d35bd8384b56635babf718cff40dbe
premek <1145361+premek@users.noreply.github.com>
2023-03-27 21:59:12 +0200

    add luacheck and shellcheck

--- test/test.sh
+++ test/test.sh
@@ -1,20 +1,42 @@
 #!/usr/bin/env sh
 
+PATTERNS="$*"
+test -z "$PATTERNS" && PATTERNS="I* lua sh"
+
 TMP="$(mktemp -d)"
 trap 'rm -rf -- "$TMP"' EXIT
-
 DIR="$(dirname "$0")"
 
+DIFF="diff"
+DIFF="colordiff"
 DIFF="delta --light --line-numbers --side-by-side"
 
 RET=0
+TESTS=0
+PASSES=0
 
-for D in $DIR/runtime/I*; do
-    TEST=$(basename "$D")
-    "./$DIR/pink-runner.lua" "$D/story.ink" < "$D/input.txt" > "$TMP/$TEST-output" &&
-       $DIFF "$D/transcript.txt" "$TMP/$TEST-output" &&
-       echo "$TEST OK"
-    if test $? -gt 0; then RET=1; fi
+for P in $PATTERNS; do
+  if [ "$P" = "lua" ]; then
+    TESTS=$((TESTS+1))      
+    find . -iname '*.lua' -exec luacheck {} + && PASSES=$((PASSES+1)) || RET=1
+    continue
+
+  elif [ "$P" = "sh" ]; then
+    TESTS=$((TESTS+1))      
+    find . -iname '*.sh' -exec shellcheck {} + && PASSES=$((PASSES+1)) || RET=1
+    continue
+
+  else
+    for D in "./$DIR/runtime/"$P; do
+      TESTCASE=$(basename "$D")
+      TESTS=$((TESTS+1))
+      "./$DIR/pink-runner.lua" "$D/story.ink" < "$D/input.txt" > "$TMP/$TESTCASE-output" &&
+         $DIFF "$D/transcript.txt" "$TMP/$TESTCASE-output" &&
+         echo "$TESTCASE OK" && PASSES=$((PASSES+1)) || RET=1
+    done
+  fi
 done
+
+echo "$PASSES/$TESTS passed"
 
 exit $RET
