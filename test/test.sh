8ca627ba256b672e188190e0ed91736d89f8b685
premek <1145361+premek@users.noreply.github.com>
2024-02-23 21:13:04 +0100

    save which tests passed

--- test/test.sh
+++ test/test.sh
@@ -23,11 +23,12 @@ DIR="$(dirname "$(dirname "$0")")"
 RET=0
 TESTS=0
 PASSES=0
+PASSED=""
 
 for P in $PATTERNS; do
   if [ "$P" = "lua" ]; then
     printf 'luacheck: '
-    TESTS=$((TESTS+1)); luacheck --codes -q . && PASSES=$((PASSES+1)) || RET=1
+    TESTS=$((TESTS+1)); luacheck --codes -q . && PASSED="$PASSED\n$P" && PASSES=$((PASSES+1)) || RET=1
 
   elif [ "$P" = "luaformat" ]; then
     echo 'luaformat...'
@@ -35,6 +36,7 @@ for P in $PATTERNS; do
       TESTS=$((TESTS+1))
       if luaformatter -s4 "$F" | diff - "$F" > /dev/null 2>&1 ; then 
           PASSES=$((PASSES+1))
+          PASSED="$PASSED\n$P:$F"
       else
           echo "$F not formatted"
           RET=1
@@ -44,12 +46,12 @@ for P in $PATTERNS; do
   elif [ "$P" = "sh" ]; then
     for F in test/*.sh; do
       TESTS=$((TESTS+1))
-      shellcheck "$F" && PASSES=$((PASSES+1)) || RET=1
+      shellcheck "$F" && PASSED="$PASSED\n$P:$F" && PASSES=$((PASSES+1)) || RET=1
     done
 
   elif [ "$P" = "api" ]; then
     TESTS=$((TESTS+1))
-    ./test/api.lua && PASSES=$((PASSES+1)) || RET=1
+    ./test/api.lua && PASSED="$PASSED\n$P" && PASSES=$((PASSES+1)) || RET=1
 
   else
     for D in "./$DIR/test/runtime/"$P; do
@@ -57,14 +59,26 @@ for P in $PATTERNS; do
       TESTS=$((TESTS+1))
       printf '%s ' "$TESTCASE" &&
          "./$DIR/pink/pink.lua" ${VERBOSE:+"$VERBOSE"} "$D/story.ink" < "$D/input.txt" 2>&1 | $DIFF "$D/transcript.txt" - &&
-         echo "OK" && PASSES=$((PASSES+1)) || RET=1
+         echo "OK" && PASSED="$PASSED\n$TESTCASE" && PASSES=$((PASSES+1)) || RET=1
     done
   fi
 done
 
 mkdir -p "test/results"
-echo "$(date --iso-8601=seconds) - $PASSES/$TESTS passed" >> "test/results/$PATTERNS.txt" # watch out * in filenames?
+D="$(date --iso-8601=seconds)"
+echo "$D - $PASSES/$TESTS passed" >> "test/results/$PATTERNS.txt" # watch out * in filenames?
+echo "$PASSED" >> "test/results/$PATTERNS-passed-$D.txt"
 
 tail "test/results/$PATTERNS.txt"
+
+LATEST="$(find "test/results/" -name "$PATTERNS-passed*" |sort|tail -1)"
+PREV="$(find "test/results/" -name "$PATTERNS-passed*" |sort|tail -2|head -1)"
+if [ -f "$LATEST" ] && [ -f "$PREV" ] ; then
+  printf '\033[1;32m' #green
+  grep -xvFf "$PREV" "$LATEST"
+  printf '\033[1;31m' #red
+  grep -xvFf "$LATEST" "$PREV" 
+  printf '\033[0m' #reset
+fi
 
 exit $RET
